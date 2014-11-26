f933f2305f92bb994a570028561fd72f1ae4e326
rezaf <reza.farajian@gmail.com>
2014-11-25 16:31:57 -0800

    finished user model and application controller

---
+++ db/migrate/20141125230211_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.integer :rating
+      t.string :session_token, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :username
+  end
+end
