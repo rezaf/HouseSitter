fd22d8610eb4cc3de3723feba490e60e30d5502a
rezaf <reza.farajian@gmail.com>
2014-11-26 15:08:38 -0800

    form data persistence added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    @credentials = {}
   end
 
   def create
     @user = User.find_by_credentials(
             params[:user][:username],
             params[:user][:password])
+
     if @user
       sign_in!(@user)
       redirect_to root_url
     else
       flash.now[:errors] = ["Invalid username and/or password"]
+      @credentials = { username: params[:user][:username] }
       render :new
     end
   end
