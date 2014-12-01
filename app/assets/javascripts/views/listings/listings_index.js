98567600844577618cf82bfdf7112cfc1b0990cb
rezaf <reza.farajian@gmail.com>
2014-12-01 09:50:19 -0800

    working with maps code

--- app/assets/javascripts/views/listings/listings_index.js
+++ app/assets/javascripts/views/listings/listings_index.js
@@ -10,3 +10,26 @@ HouseSitter.Views.ListingsIndex = Backbone.View.extend({
     return this;
   }
 });
+
+HouseSitter.Views.ListingsMap = Backbone.View.extend({
+
+  template: JST['listings/map'],
+
+  loadScript: function () {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+    'callback=initialize';
+    document.body.appendChild(script);
+  }
+
+  render: function() {
+    var renderedContent = this.template({
+      API_KEY: "AIzaSyAlgQ_SPQg1QnUHfbfpm-BJUy6N6A__U1M"
+    });
+    this.$el.html(renderedContent);
+    return this;
+  }
+});
+
+window.onload = loadScript;
