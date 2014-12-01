0897e1d31fc8c577384d42478a26ff5a34108fa2
rezaf <reza.farajian@gmail.com>
2014-12-01 11:13:50 -0800

    added map

--- app/assets/javascripts/views/listings/listings_index.js
+++ app/assets/javascripts/views/listings/listings_index.js
@@ -2,7 +2,7 @@ HouseSitter.Views.ListingsIndex = Backbone.View.extend({
 
   template: JST['listings/index'],
 
-  render: function() {
+  render: function () {
     var renderedContent = this.template({
 
     });
@@ -10,26 +10,3 @@ HouseSitter.Views.ListingsIndex = Backbone.View.extend({
     return this;
   }
 });
-
-HouseSitter.Views.ListingsMap = Backbone.View.extend({
-
-  template: JST['listings/map'],
-
-  loadScript: function () {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-    'callback=initialize';
-    document.body.appendChild(script);
-  }
-
-  render: function() {
-    var renderedContent = this.template({
-      API_KEY: "AIzaSyAlgQ_SPQg1QnUHfbfpm-BJUy6N6A__U1M"
-    });
-    this.$el.html(renderedContent);
-    return this;
-  }
-});
-
-window.onload = loadScript;
