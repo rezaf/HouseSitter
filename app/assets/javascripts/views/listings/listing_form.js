ebd07d2d3270ab4d851ed6cb396ecd09a4d4b994
rezaf <reza.farajian@gmail.com>
2014-12-01 17:07:15 -0800

    added listing form view and template

---
+++ app/assets/javascripts/views/listings/listing_form.js
@@ -0,0 +1,30 @@
+HouseSitter.Views.ListingsForm = Backbone.View.extend({
+  tagName: 'form',
+  template: JST['listings/form'],
+
+  events: {
+    'click button': 'submit'
+  },
+
+  render: function () {
+    var renderedContent = this.template({
+      post: this.model
+    });
+    this.$el.html(renderedContent);
+    return this;
+  },
+
+  submit: function (event) {
+    event.preventDefault();
+    var attrs = this.$el.serializeJSON();
+    that = this;
+
+    this.model.set(attrs);
+    this.model.save({}, {
+      success: function () {
+        that.collection.add(that.model, { merge: true });
+        Backbone.history.navigate("", { trigger: true });
+      }
+    });
+  }
+});
