2530eb3a136ade843fd51c8f1d1a6265fa8b61a9
rezaf <reza.farajian@gmail.com>
2014-12-03 17:45:03 -0800

    filepicker working

--- app/assets/javascripts/views/listings/listing_form.js
+++ app/assets/javascripts/views/listings/listing_form.js
@@ -19,18 +19,18 @@ HouseSitter.Views.ListingsForm = Backbone.View.extend({
     return this;
   },
 
-  upload: function () {
-    filepicker.pick(function(blob) {
-      return blob.url;
-    });
+  upload: function() {
+    filepicker.pick(function(blob){
+      // return(blob.url);
+      this.model.set({image: blob.url});
+    }.bind(this));
   },
 
   submit: function (event) {
     event.preventDefault();
     var attrs = this.$el.serializeJSON();
     attrs.address = attrs['address-field'];
-    attrs.image = this.upload();
-    debugger
+    // attrs.image = this.upload();
     that = this;
 
     this.model.save(attrs, {
