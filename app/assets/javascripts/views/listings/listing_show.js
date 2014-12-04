6ad8bdb2e1d00560510e6e729ad7a87a1a007215
rezaf <reza.farajian@gmail.com>
2014-12-04 13:32:52 -0800

    icon show for single items is working

--- app/assets/javascripts/views/listings/listing_show.js
+++ app/assets/javascripts/views/listings/listing_show.js
@@ -6,12 +6,19 @@ HouseSitter.Views.ListingShow = Backbone.View.extend({
     this.listenTo(this.model, 'sync', this.render)
   },
 
-  setIcon: function () {
-    var myLatlng = new google.maps.LatLng(this.model.latitude, this.model.longitude);
+  setIcon: function (latitude, longitude) {
+    var myLatlng = new google.maps.LatLng(latitude, longitude);
+    var mapOptions = {
+      zoom: 14,
+      center: myLatlng
+    }
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
     // To add the marker to the map, use the 'map' property
     var marker = new google.maps.Marker({
-      position: myLatlng
+      position: myLatlng,
+      map: map,
+      title:"Hello World!"
     });
   },
 
@@ -20,7 +27,11 @@ HouseSitter.Views.ListingShow = Backbone.View.extend({
       listing: this.model
     });
     this.$el.html(renderedContent);
-    this.setIcon();
+    var latitude = this.model.attributes.latitude;
+    var longitude = this.model.attributes.longitude;
+    if (latitude && longitude) {
+      this.setIcon(latitude, longitude);
+    }
     return this;
   }
 });
