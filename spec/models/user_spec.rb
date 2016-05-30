5c263b5420306d74cff091db4825ceb06a620dd6
rezaf <reza.farajian@gmail.com>
2016-05-30 15:40:09 -0400

    Initial user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  describe '::find_by_credentials' do
+    it "returns correct user with matching username"
+    it "returns nil if username does not match a user"
+    it "returns nil if password is not valid for user"
+  end
+
+  describe '#password=' do
+    
+  end
+
+  describe '#valid_password?' do
+
+  end
+
+  describe '#reset_session_token!' do
+
+  end
 end
