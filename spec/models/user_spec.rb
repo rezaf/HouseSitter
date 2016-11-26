4d3aafbb5a3036a4b8281bff2317086fb18d14be
rezaf <reza.farajian@gmail.com>
2016-11-25 23:25:20 -0500

    Adds more pending User model spec examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,14 +8,17 @@ describe User, type: :model do
   end
 
   describe '#password=' do
-    
+    it "sets user password"
+    it "returns user password digest"
   end
 
   describe '#valid_password?' do
-
+    it "returns true if password is valid"
+    it "returns false if password if not valid"
   end
 
   describe '#reset_session_token!' do
-
+    it "generates and saves a new session token"
+    it "returns the newly-generated session token"
   end
 end
