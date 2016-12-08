32544a70f2d8ea43891afb1da2f5069cbde8fb17
rezaf <reza.farajian@gmail.com>
2016-12-08 02:00:14 -0500

    Add User model specs

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    username "test_user"
+    email "test@example.com"
+    password "test_password"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
-describe User, type: :model do
-  describe '::find_by_credentials' do
-    it "returns correct user with matching username"
-    it "returns nil if username does not match a user"
-    it "returns nil if password is not valid for user"
+describe User do
+  let!(:user) { create(:user) }
+
+  describe '.find_by_credentials' do
+    it "returns correct user with matching username" do
+      expect(User.find_by_credentials('test_user', 'test_password')).to eq(user)
+    end
+
+    it "returns nil if username does not match a user" do
+      expect(User.find_by_credentials('test', 'test_password')).to be_nil
+    end
+
+    it "returns nil if password is not valid for user" do
+      expect(User.find_by_credentials('test_user', 'password')).to be_nil
+    end
   end
 
   describe '#password=' do
-    it "sets user password"
-    it "returns user password digest"
+    it "sets password digest" do
+      expect(user.password_digest.length).to be > 0
+      expect(user.password_digest).to be_a String
+    end
   end
 
   describe '#valid_password?' do
-    it "returns true if password is valid"
-    it "returns false if password if not valid"
+    it "returns true if password is valid" do
+      expect(user.valid_password?('test_password')).to be true
+    end
+
+    it "returns false if password if not valid" do
+      expect(user.valid_password?('test_passsword')).to be false
+    end
   end
 
   describe '#reset_session_token!' do
-    it "generates and saves a new session token"
-    it "returns the newly-generated session token"
+    it "generates and saves a new session token" do
+      original_token = user.session_token
+      user.reset_session_token!
+
+      expect(user.session_token).not_to eq original_token
+    end
+
+    it "returns the newly-generated session token" do
+      expect(user.reset_session_token!.length).to be > 0
+      expect(user.reset_session_token!).to be_a String
+    end
   end
 end
