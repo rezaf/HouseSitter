13fe659478ca4250cd5b4858a33b645b447a3d03
rezaf <reza.farajian@gmail.com>
2016-12-09 14:46:16 -0500

    Add users controller specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       sign_in!(@user)
       redirect_to root_url
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,22 +2,60 @@ require 'rails_helper'
 
 describe UsersController do
   describe '#new' do
-    it 'returns new user instance' do
-      expect(UsersController.new).to be_an_instance_of User
+    before(:each) { get :new }
+
+    it 'generates new user model instance' do
+      expect(assigns(:user)).to be_a_new User
+    end
+
+    it 'renders new user template' do
+      expect(response).to render_template(:new)
     end
   end
 
   describe '#create' do
-    let(:user) { build(:user) }
+    let(:successful_payload) {
+      {
+        username: 'username',
+        email: 'email@example.com',
+        password: 'password'
+      }
+    }
+
+    let(:unsuccessful_payload) {
+      {
+        username: 'username',
+        password: 'password'
+      }
+    }
+
+    it 'generates new user model instance' do
+      post :create, user: successful_payload
+      expect(assigns(:user)).to be_an_instance_of User
+    end
 
     context 'when user is successfuly saved' do
-      it 'signs in user'
-      it 'redirects to root URL'
+      before(:each) { post :create, user: successful_payload }
+
+      it 'signs in user' do
+        expect(session[:session_token]).to be_a String
+      end
+
+      it 'redirects to root URL' do
+        expect(response).to redirect_to :root
+      end
     end
 
     context 'when user is not successfuly saved' do
-      it 'adds error message to flash hash'
-      it 'render new view'
+      before(:each) { post :create, user: unsuccessful_payload }
+
+      it 'adds error message to flash hash' do
+        expect(flash.now[:errors]).to eq ["Email can't be blank"]
+      end
+
+      it 'renders new user template' do
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
