b49fb7943cfa0139c8098fbd4ae1a17d36ac49b5
rezaf <reza.farajian@gmail.com>
2016-12-08 23:26:15 -0500

    Add specs for listing model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    username "test_user"
-    email "test@example.com"
-    password "test_password"
+    username 'test_user'
+    email 'test@example.com'
+    password 'test_password'
+  end
+
+  factory :listing do
+    title 'Listing Title'
+    description 'Test description for listing title.'
+    user_id 1
   end
 end
---
+++ spec/models/listing_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe Listing do
+  describe 'validations' do
+    it 'validates title for presence' do
+      listing = Listing.new(description: 'description', user_id: 1)
+      expect(listing).not_to be_valid
+    end
+
+    it 'validates description for presence' do
+      listing = Listing.new(title: 'title', user_id: 1)
+      expect(listing).not_to be_valid
+    end
+
+    it 'validates user_id for presence' do
+      listing = Listing.new(title: 'title', description: 'description')
+      expect(listing).not_to be_valid
+    end
+
+    it 'passes validations for valid listing' do
+      listing = Listing.new(
+        title: 'title',
+        description: 'description',
+        user_id: 1
+      )
+
+      expect(listing).to be_valid
+    end
+  end
+end
