b727caa6560a695da1826800769ca4d528463bb5
rezaf <reza.farajian@gmail.com>
2016-12-09 15:12:11 -0500

    Adds a bunch of spec example stubs

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -2,22 +2,55 @@ require 'rails_helper'
 
 describe API::ListingsController do
   describe '#index' do
-
+    it 'sets listings to all listing records'
+    it 'renders listings json'
   end
 
   describe '#show' do
+    it 'sets listing based on ID'
 
+    context 'when listing is found' do
+      it 'renders listing json'
+    end
+
+    context 'when listing is not found' do
+      it 'renders error message json'
+    end
   end
 
   describe '#create' do
+    it 'initializes new listing'
 
+    context 'when listing sucessfully saves' do
+      it 'renders listing json'
+    end
+
+    context 'when listing does not sucessfully save' do
+      it 'renders error message json'
+    end
   end
 
   describe '#update' do
+    it 'sets listing based on ID'
 
+    context 'when listing sucessfully updates' do
+      it 'renders listing json'
+    end
+
+    context 'when listing does not sucessfully update' do
+      it 'renders error message json'
+    end
   end
 
   describe '#destroy' do
+    it 'sets listing based on ID'
 
+    context 'when listing sucessfully destroyed' do
+      it 'renders destroyed message'
+    end
+
+    context 'when listing does not sucessfully destroy' do
+      it 'renders error message json'
+    end
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 describe SessionsController do
   describe '#new' do
-
+    it 'sets credentials to an empty hash'
+    it 'sets trigger_modal to false'
   end
 
   describe '#create' do
+    it 'finds and assigns user instance'
 
+    context 'when user is found' do
+      it 'signs in user'
+      it 'redirects to root url'
+    end
+
+    context 'when user is not found' do
+      it 'adds error message to flash hash'
+      it 'updates credentials username value'
+      it 'sets trigger_modal to true'
+      it 'renders new user tempalate'
+    end
   end
 
   describe '#guest_sign_in' do
-
+    it 'assigns user instance to guest user'
+    it 'signs in guest user'
+    it 'redirects to root url'
   end
 
   describe '#destroy' do
-
+    it 'signs out user'
+    it 'redirects to new session url'
   end
 end
