a1ddb24cd01250da235e5500b8041073e2c80c6f
rezaf <reza.farajian@gmail.com>
2016-12-09 14:49:49 -0500

    Create sessions and listings controller spec skeletons

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-describe ListingsController do
-  
+describe API::ListingsController do
+  describe '#index' do
+
+  end
+
+  describe '#show' do
+
+  end
+
+  describe '#create' do
+
+  end
+
+  describe '#update' do
+
+  end
+
+  describe '#destroy' do
+
+  end
 end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe SessionsController do
+  describe '#new' do
+
+  end
+
+  describe '#create' do
+
+  end
+
+  describe '#guest_sign_in' do
+
+  end
+
+  describe '#destroy' do
+
+  end
+end
