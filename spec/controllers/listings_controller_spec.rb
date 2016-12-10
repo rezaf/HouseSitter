894963896769a7cd89ac69a24bafafe71c8a5a03
rezaf <reza.farajian@gmail.com>
2016-12-09 22:32:33 -0500

    Add listings controller index action specs

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -1,56 +1,94 @@
 require 'rails_helper'
 
-describe API::ListingsController do
+describe Api::ListingsController do
   describe '#index' do
-    it 'sets listings to all listing records'
-    it 'renders listings json'
+    let!(:current_user) { create(:user, :guest) }
+    let!(:all_listings) { create_list(:listing, 3) }
+
+    before(:each) do
+      session[:session_token] = current_user.session_token
+      get :index
+    end
+
+    it 'sets listings to all listing records' do
+      expect(assigns(:listings)).to eq all_listings
+    end
+
+    it 'renders listings as json' do
+      expect(response.content_type).to eq 'application/json'
+      expect(JSON.parse(response.body).length).to be 3
+    end
   end
 
   describe '#show' do
-    it 'sets listing based on ID'
+    it 'sets listing based on ID' do
+
+    end
 
     context 'when listing is found' do
-      it 'renders listing json'
+      it 'renders listing json' do
+
+      end
     end
 
     context 'when listing is not found' do
-      it 'renders error message json'
+      it 'renders error message json' do
+
+      end
     end
   end
 
   describe '#create' do
-    it 'initializes new listing'
+    it 'initializes new listing' do
+
+    end
 
     context 'when listing sucessfully saves' do
-      it 'renders listing json'
+      it 'renders listing json' do
+
+      end
     end
 
     context 'when listing does not sucessfully save' do
-      it 'renders error message json'
+      it 'renders error message json' do
+
+      end
     end
   end
 
   describe '#update' do
-    it 'sets listing based on ID'
+    it 'sets listing based on ID' do
+
+    end
 
     context 'when listing sucessfully updates' do
-      it 'renders listing json'
+      it 'renders listing json' do
+
+      end
     end
 
     context 'when listing does not sucessfully update' do
-      it 'renders error message json'
+      it 'renders error message json' do
+
+      end
     end
   end
 
   describe '#destroy' do
-    it 'sets listing based on ID'
+    it 'sets listing based on ID' do
+
+    end
 
     context 'when listing sucessfully destroyed' do
-      it 'renders destroyed message'
+      it 'renders destroyed message' do
+
+      end
     end
 
     context 'when listing does not sucessfully destroy' do
-      it 'renders error message json'
+      it 'renders error message json' do
+
+      end
     end
   end
 end
